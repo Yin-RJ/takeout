6edb7ccdb12fe367e3f2f77a79c54bceab873c94
YinRJ <yin_rj@qq.com>
2021-08-17 19:51:21 +0800

    [订餐系统] 搭建整体模块，分为商家微服务、订单微服务以及骑手微服务

---
+++ orderservice/src/main/java/com/yinrj/OrderServiceApplication.java
@@ -0,0 +1,19 @@
+package com.yinrj;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yinrongjie
+ * @version 1.0
+ * @description
+ * @date 2021/8/17
+ */
+@SpringBootApplication
+@MapperScan("com.yinrj")
+public class OrderServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
+}
